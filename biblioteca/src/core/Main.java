ad322728c4f568427ca1a38db8c0a04bd772e104
Andrew Mikael de Souza Rodrigues <47177577+andrewgames520@users.noreply.github.com>
2026-03-22 16:15:35 -0300

    Upload do começo do codigo

---
+++ biblioteca/src/core/Main.java
@@ -0,0 +1,32 @@
+package core;
+
+import entidades.Livro;
+import entidades.Usuario;
+import entidades.Biblioteca;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Biblioteca biblioteca = new Biblioteca();
+		Livro livro1 = new Livro();
+		Livro livro2 = new Livro();
+		Livro livro3 = new Livro();
+		
+		livro1.setTitulo("O mochileiro das galaxias");
+		livro2.setTitulo("O Imperador amarelo");
+		livro3.setTitulo("A garota que roubava livros");
+		
+		livro1.setQntdisponivel(4);
+		livro2.setQntdisponivel(2);
+		livro3.setQntdisponivel(8);
+		
+		biblioteca.addlivro(livro1);
+		biblioteca.addlivro(livro2);
+		biblioteca.addlivro(livro3);
+		
+		biblioteca.listarLivros();
+	
+
+	}
+
+}
---
+++ biblioteca/src/entidades/Usuario.java
@@ -0,0 +1,32 @@
+package entidades;
+
+import java.util.Date;
+
+public class Usuario {
+	private String nome;
+	private String registro;
+	private Date dataNasc;
+	
+	//criar metodo construtor
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getRegistro() {
+		return registro;
+	}
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+	public void setDataNasc(Date dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	
+	
+}
